ea89ebd79bb9e6bfef4837133bb3018b8b6a360b
pedram <peeeedii@gmail.com>
2024-11-01 22:22:26 +0330

    chore: remove unused url path

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    # path("login/", views.user_login, name='login')
     path("login/", auth_views.LoginView.as_view(), name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name='logout'),
-
     path("", views.dashboard, name='dashboard')
 ]
