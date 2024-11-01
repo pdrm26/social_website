8136a1011b03411d9e3546cb8de0e8d1612d8ac8
pedram <peeeedii@gmail.com>
2024-11-02 00:03:11 +0330

    feat: add edit page and attach profile model to user model

--- account/views.py
+++ account/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from .forms import UserRegistrationForm
+from .forms import (
+    UserRegistrationForm,
+    UserEditForm,
+    ProfileEditForm
+)
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 @login_required
@@ -17,8 +22,42 @@ def register(request):
             # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+
+            # Create the user profile
+            Profile.objects.create(user=new_user)
+
             return render(request, "account/register_done.html", {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
 
     return render(request, "account/register.html", {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+    )
