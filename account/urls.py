9a9611dae06a4b04d6c9862a9e03009a76b8ebcf
pedram <peeeedii@gmail.com>
2024-11-01 22:29:22 +0330

    feat: add change password page

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,10 @@ from . import views
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name='logout'),
-    path("", views.dashboard, name='dashboard')
+    path("password-change/", auth_views.PasswordChangeView.as_view(),
+         name="password-change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
+    path("", views.dashboard, name='dashboard'),
 ]
